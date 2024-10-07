411c02cc86bcb3d4bb2d0bfca364d108bd22b019
jackloosantamaria <jackloosantamaria@hotmail.com>
2024-10-07 16:12:40 -0500

    learning activity week 4

---
+++ routes/accountRoute.js
@@ -0,0 +1,26 @@
+//importing modules
+
+const regValidate = require('../utilities/account-validation')
+const express = require('express');
+const router = express.Router();
+const {handleErrors} = require('../utilities');
+const accountsController = require('../controllers/accountsController');
+const utilities = require("../utilities")
+
+//Define GET router for "My Account"
+router.get('/login', accountsController.buildLogin);
+
+//Define GET router for "Register"
+router.get('/register', accountsController.buildRegister);
+
+router.post('/register',
+    regValidate.registrationRules(),
+    regValidate.checkRegData, 
+    utilities.handleErrors(accountsController.registerAccount))
+
+//Add error handling middleware
+router.use((err, res, next) => {
+    handleErrors(err, req, res);
+});
+
+module.exports = router;
